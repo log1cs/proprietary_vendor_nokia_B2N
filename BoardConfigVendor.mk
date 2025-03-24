86b7b3c09a5f7ad24f1cdc6f9c84c7ab427f88a5
Tuan Anh <tuan73176@gmail.com>
2025-03-24 22:18:15 +0700

    B2N: Initial proprietary vendor blobs
    Change-Id: I8f0b8729b0d674c0603d4850f07e1bfa3c6007f3

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,24 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    bluetooth \
+    cda \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    hidden \
+    hyp \
+    keymaster \
+    mdtp \
+    mdtpsecapp \
+    modem \
+    nvdef \
+    pmic \
+    rpm \
+    systeminfo \
+    tz \
+    xbl
